649e2fa3f31275e6ac1b39f06e5bacc676287a4d
Lajos Kormos <lajos.kormos@lhsystems.com>
2022-12-09 15:19:25 +0100

    Fix "User already logged in" issue Check if there is an authTokenand handle the routing based on that

--- zarofeladat/src/App.js
+++ zarofeladat/src/App.js
@@ -1,22 +1,30 @@
 import * as React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Providers from './Providers';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAuth } from './hooks/useAuth';
 import LoginScreen from './screens/LoginScreen';
 import MyWalletsScreen from './screens/MyWalletsScreen';
 import WalletScreen from './screens/WalletScreen';
 import WalletsScreen from './screens/WalletsScreen';
 
 function App() {
-  return (
-    <Providers>
+  const { authToken } = useAuth();
+
+  if (authToken) {
+    return (
       <Routes>
-        <Route element={<LoginScreen />} path="/" />
         <Route element={<WalletsScreen />} path="/wallets" />
         <Route element={<MyWalletsScreen />} path="/mywallets" />
         <Route element={<WalletScreen />} path="wallet/:id" />
+        <Route path="/" element={<Navigate to="/wallets" replace />} />
         <Route element={<h1>404 - Not found</h1>} path="*" />
       </Routes>
-    </Providers>
+    );
+  }
+  return (
+    <Routes>
+      <Route element={<LoginScreen />} path="/" />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
